6a38e65e6f0731adfbca2372e8a58c7744906c86
Christian McCabe <christian.mccabe89@gmail.com>
2022-09-14 13:14:16 -0600

    Complete friends.rb exercise

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Eric", "Sean", "Patrick", "Marcus", "Ilana"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ["London", "France", "Thailand", "Mombai", "Iceland"]
+places.shift(2)
+puts places
